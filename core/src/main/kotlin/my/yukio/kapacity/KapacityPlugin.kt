37f93cd527cc0f1d8f1015b49fc88cb15da1058d
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2022-02-11 22:51:20 +0300

    Support multiple files

--- core/src/main/kotlin/my/yukio/kapacity/KapacityPlugin.kt
+++ core/src/main/kotlin/my/yukio/kapacity/KapacityPlugin.kt
@@ -5,12 +5,31 @@ import arrow.meta.Meta
 import arrow.meta.invoke
 import arrow.meta.quotes.Transform
 import arrow.meta.quotes.classDeclaration
+import java.io.File
 
 val Meta.addShallowSize: CliPlugin
     get() = "Add shallow size to the data classes" {
-        meta(
-            classDeclaration(this, { element.name == "AddShallowSize" }) {
-                Transform.newSources()
-            }
-        )
+        val classesExtensionPhase = classDeclaration(this, { element.isData() && element.isTopLevel() }) {
+            val classPackage = value.fqName?.parent()?.toString() ?: ""
+            val className = name?.identifier ?: error("")
+            val nFields = value.primaryConstructorParameters.size
+            val bytesInField = 8
+            val fileContent = genFileContent(classPackage, className, nFields * bytesInField)
+            val file = fileContent.file(
+                fileName = "${className}.kt",
+                filePath = classPackage.replace('.', File.separatorChar)
+            )
+            Transform.newSources(file)
+        }
+        meta(classesExtensionPhase)
     }
+
+private fun genFileContent(classPackage: String, className: String, shallowSize: Int): String =
+    """
+    |package $classPackage
+    |
+    |import ${classPackage}.$className
+    |
+    |val ${className}.shallowSize: Int
+    |    get() = $shallowSize
+    """.trimMargin()
--- example/src/main/kotlin/my/yukio/kapacity/example/Main.kt
+++ example/src/main/kotlin/my/yukio/kapacity/example/Main.kt
@@ -1,5 +1,18 @@
 package my.yukio.kapacity.example
 
+import my.yukio.kapacity.example.shallowSize
+
+data class Tmp(val x: Int = 0) {
+    val y: Int
+        get() = 42
+
+    val z: Int
+        get() = 10
+}
+
+data class Q(val x: Int = 1, val y: Long = 2)
+
 fun main() {
-    println("Hello world")
+    println(Tmp().shallowSize)
+    println(Q().shallowSize)
 }
